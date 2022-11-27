9377df932d16868b55320479a52a928fd046188f
valetronika <valentina2wo@gmail.com>
2022-11-27 13:59:12 +0100

    HM 25 nov

--- src/HW/HomeWork25nov.java
+++ src/HW/HomeWork25nov.java
@@ -5,14 +5,25 @@ import java.util.Set;
 
 public class HomeWork25nov {
     public static void main(String[] args) {
-        String[] levelOne = new String[100];
+        String[] levelOne = new String[100];//UPD я прочитала задание как 10,1 в первую добавить...
         levelOne[0] = String.valueOf(10.1);
         for (int i = 1; i < 100; i++) {
             levelOne[i] = String.valueOf(0);
         }
         System.out.println(Arrays.toString(levelOne));
     }
-//Level Two-------------------
+
+    //UPD Level One
+    {
+        int[] levelOneUpdate = new int[100];
+        levelOneUpdate[0] = 10;
+        levelOneUpdate[99] = 10;
+        for (int i = 1; i < 98; i++) {
+            levelOneUpdate[i] = 0;
+            System.out.println(Arrays.toString(levelOneUpdate));
+        }
+    }
+    //Level Two-------------------
     {
         String[] levelTwo = new String[100];
         levelTwo[0] = String.valueOf(0);
