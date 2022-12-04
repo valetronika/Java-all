28733ed3203f1d5872a8d84a39fc13a1bd0304cb
valetronika <115985167+valetronika@users.noreply.github.com>
2022-12-04 17:13:28 +0100

    Add files via upload

---
+++ src/HW/MainTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void testSum() {
+        int a = Main.getAdd(10, 5);
+        assertEquals(15, a);
+
+    }
+
+    @Test
+    public void testMultiply() {
+        int a = Main.getMultiply(10, 5);
+        assertEquals(50, a);
+    }
+@Test
+    public void testDiv() {
+        int a = Main.getDivide(10, 5);
+        assertEquals(2, a);
+    }
+@Test
+    public void testSub() {
+        int a = Main.getSubtract(10,5);
+        assertEquals( 5,a);
+    }
+}
